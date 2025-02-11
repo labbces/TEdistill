455e2ac1b3984a03936f45713bd41907d8b6b37a
Diego M Riano-Pachon <diriano@gmail.com>
2025-02-11 13:06:28 -0300

    Adding rules for classification

--- class_table.py
+++ class_table.py
@@ -32,7 +32,61 @@ def main():
 			
 			elif columns[1] == "LTR/Gypsy" and columns[2] == "ClassI LTR Gypsy" and columns[3].startswith("LTR/Gypsy"):
 				print(f'{columns[0]}\t{columns[3]}', file=o)
-
+			elif columns[1] == "Unknown" and columns[2] == "ClassI LTR Gypsy" and columns[3].startswith("LTR/Gypsy"):
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "LTR/Gypsy" and columns[2] == "ClassI LTR" and columns[3].startswith("LTR/Gypsy"):
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "LTR/Copia" and columns[2] == "ClassI LTR Copia" and columns[3].startswith("LTR/Copia"):
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "Unknown" and columns[2] == "ClassI LTR Copia" and columns[3].startswith("LTR/Copia"):
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "LTR/Copia" and columns[2] == "ClassI LTR" and columns[3].startswith("LTR/Copia"):
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "LTR/Copia" and columns[2] == "ClassI LTR Gypsy" and columns[3].startswith("LTR/Copia"):
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "LTR/Gypsy" and columns[2] == "ClassI LTR Copia" and columns[3].startswith("LTR/Gypsy"):
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "LTR/Gypsy" and columns[2] == "ClassI LTR Copia" and columns[3].startswith("LTR/Gypsy"):
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "LTR/Gypsy" and columns[2] == "ClassI LTR Copia" and columns[3].startswith("LTR/Gypsy"):
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "DNA/CMC-EnSpm" and columns[2].startswith("ClassII DNA CACTA") and columns[3].startswith("TIR/EnSpm_CACTA/"):#EnSpm_CACTA is the same as CMC-EnSpm following https://www.jstage.jst.go.jp/article/ggs/94/6/94_18-00024/_html/-char/en
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "DNA/CMC-EnSpm" and 'CACTA' not in columns[2] and columns[2].startswith("ClassII DNA ") and columns[3].startswith("TIR/EnSpm_CACTA/"):#EnSpm_CACTA is the same as CMC-EnSpm following https://www.jstage.jst.go.jp/article/ggs/94/6/94_18-00024/_html/-char/en
+				print(f'{columns[0]}\t{columns[3]}', file=o)
+			elif columns[1] == "DNA/CMC-EnSpm" and columns[2].startswith("ClassII DNA CACTA") and columns[3] == 'Unknown':#EnSpm_CACTA is the same as CMC-EnSpm following https://www.jstage.jst.go.jp/article/ggs/94/6/94_18-00024/_html/-char/en
+				print(f'{columns[0]}\tTIR/EnSpm_CACTA/unknown', file=o)
+			elif columns[1].startswith("DNA/hAT-") and columns[2].startswith("ClassII DNA hAT ") and (columns[3].startswith("TIR/hAT/") or columns[3] == "Unknown"):
+				if columns[3] == "Unknown":
+					col3sub = 'unknown'
+				else:
+					col3sub = columns[3].split("/")[2]
+				
+				col1sub = columns[1].split("-")[1]
+				col2sub= columns[2].split(" ")[3]
+				
+				if(col1sub == col2sub and col2sub == col3sub):
+					newclassif='TIR/hAT/'+col1sub
+				elif(col1sub == col2sub and col3sub == 'unknown'):
+					newclassif='TIR/hAT/'+col1sub
+				elif(col1sub != col3sub and col1sub != col2sub):
+					newclassif='TIR/hAT/unknown'
+				elif(col1sub != col3sub and col3sub == col2sub):
+					newclassif='TIR/hAT/'+col2sub
+				elif(col1sub == col2sub and col1sub != col3sub):
+					newclassif='TIR/hAT/'+col1sub
+				else:
+					newclassif='TIR/hAT/unknown'
+				print(f'{columns[0]}\t{newclassif}', file=o)
+			elif columns[1] == 'Unknown' and columns[3] == 'Unknown' and (columns[2].startswith("ClassII DNA") and columns[2].endswith("MITE")):
+				if 'hAT' in columns[2]:
+					print(f'{columns[0]}\tTIR/hAT/unknown *MITE', file=o) #TODO revisar se é correcto, MITe vs nMITE https://academic.oup.com/bioinformatics/article/36/15/4269/5838183
+				elif 'TcMar' in columns[2]:
+					print(f'{columns[0]}\tTIR/Tc1/Mariner *MITE', file=o) #TODO revisar se é correcto, MITe vs nMITE https://academic.oup.com/bioinformatics/article/36/15/4269/5838183
+				elif 'Mutator' in columns[2]:
+					print(f'{columns[0]}\tTIR/MuDR/Mutator *MITE', file=o) #TODO revisar se é correcto, MITe vs nMITE https://academic.oup.com/bioinformatics/article/36/15/4269/5838183
+				elif 'Harbinger' in columns[2]:
+					print(f'{columns[0]}\tTIR/PIF/Harbinger *MITE', file=o) #TODO revisar se é correcto, MITe vs nMITE https://academic.oup.com/bioinformatics/article/36/15/4269/5838183							
 			else:
 				print(line)
 
