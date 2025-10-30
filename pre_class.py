4a4a10aefad0218ce24f71b002abb3d52ecc6724
am-cunha <andreza.mcunha@gmail.com>
2025-10-30 10:03:13 -0300

    Updated sequence ontology file (with DeepTE classification nomenclature) and pre-classification script

--- pre_class.py
+++ pre_class.py
@@ -1,60 +1,29 @@
 import argparse
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Padronize files classification nomenclature.")
-    parser.add_argument('--EarlGrey', type=str, required=True, help='Earl Grey output file path')
-    parser.add_argument('--DeepTE', type=str, required=True, help='DeepTE output file path')
-    parser.add_argument('--TEsorter', type=str, required=True, help='TEsorter output file path')
-    parser.add_argument('--TEtrimmer', type=str, required=True, help='TEtrimmer output file path')
-    parser.add_argument('--RepClassifier', type=str, required=True, help='RepeatClassifier output file path')
-    parser.add_argument('--MITEtracker', type=str, required=True, help='MITEtracker output file path')
-    parser.add_argument('--output', type=str, required=True, help='Output file path')
+	parser = argparse.ArgumentParser(description="Padronize files classification nomenclature.")
+	parser.add_argument('--Onthology', type=str, required=True, help='Sequence onthology file with all nomenclature from selected tools')
+	parser.add_argument('--EarlGrey', type=str, required=True, help='Earl Grey output file path')
+	parser.add_argument('--DeepTE', type=str, required=True, help='DeepTE output file path')
+	parser.add_argument('--TEsorter', type=str, required=True, help='TEsorter output file path')
+	parser.add_argument('--TEtrimmer', type=str, required=True, help='TEtrimmer output file path')
+	parser.add_argument('--RepClassifier', type=str, required=True, help='RepeatClassifier output file path')
+	parser.add_argument('--MITEtracker', type=str, required=True, help='MITEtracker output file path')
+	parser.add_argument('--output', type=str, required=True, help='Output file path')
 
-    args = parser.parse_args()
-    return args
+	args = parser.parse_args()
+	return args
 
 def normalize_classification():
 
-    # Define dictionary for nomenclature normalization
-    classification = {ClassI: [eg(), dte(0-11), tes(), tet(), rc(), mt()],
-                      ClassII: [eg(), dte(12-19), tes(), tet(), rc(), mt()]}
-    
-    # Lists classification names and categories per tool from ClassI RNA transposons to ClassII DNA transposons
-    eg = []
-
-    dte = [ClassI,
-           ClassI_LTR,
-           ClassI_LTR_Copia,
-           ClassI_LTR_Gypsy,
-           ClassI_nLTR,
-           ClassI_nLTR_DIRS,
-           ClassI_nLTR_LINE,
-           ClassI_nLTR_LINE_I,
-           ClassI_nLTR_LINE_L1,
-           ClassI_nLTR_PLE,
-           ClassI_nLTR_SINE,
-           ClassI_nLTR_SINE_7SL,
-           ClassI_nLTR_SINE_tRNA,
-           ClassII_sub1,
-           ClassII_sub1_DNA_CACTA,
-           ClassII_sub1_DNA_Harbinger,
-           ClassII_sub1_DNA_hAT,
-           ClassII_sub1_DNA_Mutator,
-           ClassII_sub1_DNA_P,
-           ClassII_sub1_DNA_TcMar,
-           ClassII_sub2_Helitron
-           #Domain model: _MITE | _nMITE
-           ]
-
-    tes = []
-
-    tet = []
-
-    rc = []
-
-    mt = []
+	# Define dictionary for nomenclature normalization using the seq_ontology.txt
+	classification = {
+			ClassI: {eg(), dte(), tes(), rc(), mt()},
+			ClassII: {eg(), dte(), tes(), tet(), rc(), mt()}
+			}
 
 def main():
-    args = parse_arguments()
-    normalize_classification()
-    # Further processing and file handling would go here
+	args = parse_arguments()
+	normalize_classification()
+
+	# Further processing and file handling would go here
